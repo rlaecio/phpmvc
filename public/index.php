635143a64bcee6dbbd28de78bf65ffab24f39545
Roque Laecio dos Santos <rlaecio@hotmail.com>
2021-02-13 01:05:47 +0000

    Adicionando Rotas

--- public/index.php
+++ public/index.php
@@ -1,28 +1,18 @@
 <?php
 
-use Alura\Cursos\Controller\FormularioInsercao;
-use Alura\Cursos\Controller\ListarCursos;
-use Alura\Cursos\Controller\Persistencia;
+use Alura\Cursos\Controller\InterfaceControladorRequisicao;
 
 require __DIR__ . '/../vendor/autoload.php';
+$caminho = $_SERVER['PATH_INFO'];
+$rotas = require __DIR__ . '/../config/routes.php';
 
-switch ($_SERVER['PATH_INFO']) {
-    case '/listar-cursos':
-        $controlador = new ListarCursos();
-        $controlador->processaRequisicao();
-        break;
+if (!array_key_exists($caminho, $rotas)) {
+    echo "Erro 404";
+    exit();
+}
 
-    case '/novo-curso':
-        $controlador = new FormularioInsercao();
-        $controlador->processaRequisicao();
-        break;
+$classeControladora = $rotas[$caminho];
 
-    case '/salvar-curso':
-        $controlador = new Persistencia();
-        $controlador->processaRequisicao();
-        break;
-        
-    default:
-        echo "Erro 404";
-        break;
-    }
+/** @var InterfaceControladorRequisicao $controlador  */
+$controlador = new $classeControladora();
+$controlador->processaRequisicao();
